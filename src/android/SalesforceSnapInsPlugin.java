fb9ade50d3d98fe14e8c2c836d0d58810b0d4a90
Pedro Gutiérrez <p.gutierrez@cognifit.com>
2019-06-20 15:42:02 +0200

    Added missing imports for Map and HashMap

--- src/android/SalesforceSnapInsPlugin.java
+++ src/android/SalesforceSnapInsPlugin.java
@@ -39,7 +39,8 @@ import com.salesforce.android.chat.ui.model.PreChatPickListField;
 
 import java.util.ArrayList;
 import java.util.List;
-
+import java.util.Map;
+import java.util.HashMap;
 
 public class SalesforceSnapInsPlugin extends CordovaPlugin {
 
